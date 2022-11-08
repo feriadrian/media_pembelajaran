086e5257c3f0632b3e55ef7a91abe47fbc14c832
Feri Adrian <feritarakan@gmail.com>
2022-11-08 15:22:00 +0800

    fix widget testing

--- test/widget/widget_testing.dart
+++ test/widget/widget_testing.dart
@@ -1,23 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:mini_projeck/constant/constant.dart';
+import 'package:mini_projeck/main.dart';
 import 'package:mini_projeck/pages/home_page/home_page.dart';
 import 'package:mini_projeck/pages/spalsh_page/splash_page.dart';
 
 void main() {
-  testWidgets('finds a scaffold widget', (tester) async {
-    await tester.pumpWidget(
-      HomePage(),
-    );
-
-    expect(find.byWidget(Scaffold()), findsOneWidget);
+  testWidgets('Find TextFiled', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+    var textFiled = find.byType(SplashPage);
+    expect(textFiled, findsOneWidget);
+  });
+  testWidgets('Find Widget Scaffold At HomePage', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+    var scaffold = find.byType(Scaffold);
+    expect(scaffold, findsOneWidget);
   });
 
-  testWidgets('finds a specific instance', (tester) async {
-    var childWidget = Padding(padding: EdgeInsets.all(defaultPadding));
+  testWidgets('Find Text ', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
+    expect(find.text('Ayo Mulai Belajar'), findsOneWidget);
+  });
 
-    await tester.pumpWidget(Container(child: childWidget));
+  testWidgets('Find Text', (WidgetTester tester) async {
+    await tester.pumpWidget(MyApp());
 
-    expect(find.byWidget(childWidget), findsOneWidget);
+    expect(find.text('Silahkan Pilihan Materi Dibawah'), findsOneWidget);
   });
 }
